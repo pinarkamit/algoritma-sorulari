ae3f84dfcb11cbc585ee92a20b09ec8bc2fb0298
Pınar Kamit <pinarkamit@gmail.com>
2022-02-16 12:26:34 +0300

    odev1_2

---
+++ odev1_2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Odev1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two positive numbers: ");
+            int[] arr2 = new int[2];
+            EnterMultipleIntegers(ref arr2, 2);
+            int n = arr2[0];
+            int m = arr2[1];
+            Console.WriteLine("Enter positive numbers " + n + " times: ");
+            int[] arrN = new int[n];
+            EnterMultipleIntegers(ref arrN, n);
+            for (int i = 0; i < arrN.Length; i++)
+            {
+                if (arrN[i] % m == 0)
+                    Console.Write(arrN[i] + " ");
+            }
+        }
+
+        private static void EnterMultipleIntegers(ref int[] arr2, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                int res = EnterInteger();
+                if (res <= 0)
+                {
+                    Console.WriteLine("You must enter positive values: ");
+                    i--;
+                    continue;
+                }
+                arr2[i] = res;
+            }
+        }
+
+        private static int EnterInteger()
+        {
+            int outNumber = 0;
+            string? str = Console.ReadLine();
+            bool isInteger = Int32.TryParse(str, out outNumber);
+            int n = 0;      
+            if (isInteger)
+            {
+                n = Convert.ToInt32(str?.Trim());
+            }
+            return n;
+        }
+    }
+}
