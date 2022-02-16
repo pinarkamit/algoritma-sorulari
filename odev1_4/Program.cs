09f249466cc29dd4d4879790c745f94d1d0341fd
Pınar Kamit <pinarkamit@gmail.com>
2022-02-16 18:08:50 +0300

    odev1_4

---
+++ odev1_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Odev1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string? sentence = Console.ReadLine();
+            string[] words = new string[sentence.Length];
+            words = sentence.Split(" ");
+            Console.WriteLine("Total word count in sentence: " + words.Length);
+            string sentenceNonSpace = sentence.Trim().Replace(" ", "");
+            Console.WriteLine("Total char coutn in sentence: " +sentenceNonSpace.Length);
+        }
+    }
+}
